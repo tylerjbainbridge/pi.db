9e32f7bb83272fdb7e8d467ff249c25f6c7d4cda
Tyler Bainbridge <tylerbainbridge@MacBook-Pro.local>
2021-07-22 12:25:08 -0400

    case insensative

--- api.ts
+++ api.ts
@@ -18,11 +18,11 @@ client.on('message', (msg) => {
 
   if (
     msg.author.id === process.env.TYLER_USER_ID &&
-    msg.content?.toLowerCase() === 'pi sync'
+    msg.content?.toLowerCase()?.trim() === 'pi sync'
   ) {
   }
 
-  if (msg.content?.toLowerCase() === 'pi rec') {
+  if (msg.content?.toLowerCase()?.trim() === 'pi rec') {
     console.log('responding with rec!');
 
     const rec = _.sample(recs);
