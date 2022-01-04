606aaa57e96d360cf0b37858ad37e5ff14726aa9
Tyler Bainbridge <tbainbridge@fb.com>
2022-01-04 17:30:10 -0500

    discord rec search

--- api.ts
+++ api.ts
@@ -1,16 +1,16 @@
-import * as Discord from 'discord.js';
-import _ from 'lodash';
-require('dotenv').config();
-import express from 'express';
+import * as Discord from "discord.js";
+import _ from "lodash";
+require("dotenv").config();
+import express from "express";
 const app = express();
 const port = 3000;
-import prisma from './lib/prisma';
+import prisma from "./lib/prisma";
 
-import next from 'next';
-import { SYNC_STATUS, syncDB, getFailedURLs } from './scraper/sync-utils';
-import { Feature, Guest, Rec } from '@prisma/client';
+import next from "next";
+import { SYNC_STATUS, syncDB, getFailedURLs } from "./scraper/sync-utils";
+import { Feature, Guest, Rec } from "@prisma/client";
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
@@ -22,35 +22,35 @@ type ResultRec = Rec & {
 const client = new Discord.Client();
 let recs: ResultRec[] = [];
 
-client.on('ready', async () => {
-  console.log('ready!!');
+client.on("ready", async () => {
+  console.log("ready!!");
 });
 
-client.on('message', async (msg) => {
+client.on("message", async (msg) => {
   const content = msg.content?.toLowerCase()?.trim();
 
-  if (!content.startsWith('pi')) {
+  if (!content.startsWith("pi")) {
     return;
   }
 
   if (
     msg.channel.id === process.env.ADMIN_DATA_CHANNEL_ID &&
-    (content === 'pi sync' || content === 'pi sync retry')
+    (content === "pi sync" || content === "pi sync retry")
   ) {
-    if (SYNC_STATUS === 'ACTIVE') {
-      msg.reply('Sync already in progress.');
+    if (SYNC_STATUS === "ACTIVE") {
+      msg.reply("Sync already in progress.");
       return;
     }
 
     let logData;
 
-    if (content === 'pi sync retry') {
+    if (content === "pi sync retry") {
       logData = await syncDB(false, await getFailedURLs());
     } else {
       logData = await syncDB();
     }
 
-    msg.reply('Starting sync...');
+    msg.reply("Starting sync...");
     try {
       msg.reply(
         `Synced ${logData.syncedUrlsCount} features in ${logData.seconds} seconds.`
@@ -58,7 +58,7 @@ client.on('message', async (msg) => {
 
       msg.reply(JSON.stringify(logData, null, 5));
     } catch (e) {
-      msg.reply('Something went wrong while syncing...');
+      msg.reply("Something went wrong while syncing...");
     }
 
     recs = await prisma.rec.findMany({
@@ -68,12 +68,14 @@ client.on('message', async (msg) => {
       },
     });
 
-    msg.reply('Done.');
+    msg.reply("Done.");
 
     return;
   }
 
-  if (content === 'pi rec') {
+  if (content === "pi rec") {
+    const query = content.split("pi rec").filter(Boolean).pop();
+
     if (recs.length === 0) {
       recs = await prisma.rec.findMany({
         include: {
@@ -83,13 +85,23 @@ client.on('message', async (msg) => {
       });
     }
 
-    const rec = _.sample(recs);
+    const filteredRecs =
+      query && query.length > 0
+        ? recs.filter((rec) => {
+            return [rec.title, rec.content, rec.guest.name]
+              .join("")
+              .toLowerCase()
+              .includes(query.toLowerCase());
+          })
+        : recs;
 
-    console.log('responding with rec!', rec?.id);
+    const rec = _.sample(filteredRecs);
+
+    console.log("responding with rec!", rec?.id);
 
     if (rec != null) {
       const embed = new Discord.MessageEmbed()
-        .setTitle([rec?.emoji, rec?.title].filter(Boolean).join(' '))
+        .setTitle([rec?.emoji, rec?.title].filter(Boolean).join(" "))
         // .setAuthor("Perfectly Imperfect", "https://cdn.substack.com/image/fetch/w_1360,c_limit,f_auto,q_auto:best,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fecbe78f0-ea9f-4221-8db1-6d10269a5c80_1000x1019.png")
 
         .setDescription(rec?.content);
@@ -104,7 +116,7 @@ client.on('message', async (msg) => {
       embed
         .addFields({ name: "Rec'd by", value: rec?.guest?.name, inline: true })
         .addFields({
-          name: 'Feature',
+          name: "Feature",
           value: rec?.feature?.url,
           inline: true,
         })
@@ -121,8 +133,8 @@ client.on('message', async (msg) => {
   }
 });
 
-client.on('error', (e) => {
-  console.log('something went wrong');
+client.on("error", (e) => {
+  console.log("something went wrong");
   console.log(e);
 });
 
@@ -131,16 +143,16 @@ client.login(process.env.BOT_TOKEN);
 (async () => {
   try {
     await nextApp.prepare().then(() => {
-      app.get('/api/health-check', (_req, res) => {
+      app.get("/api/health-check", (_req, res) => {
         res.sendStatus(200);
       });
 
-      app.get('/api/recs', async (_req, res) => {
+      app.get("/api/recs", async (_req, res) => {
         res.json({
           recs: await prisma.rec.findMany({
             orderBy: [
               {
-                date: 'asc',
+                date: "asc",
               },
             ],
             include: {
@@ -151,7 +163,7 @@ client.login(process.env.BOT_TOKEN);
         });
       });
 
-      app.get('*', (req, res) => {
+      app.get("*", (req, res) => {
         return handle(req, res);
       });
 
